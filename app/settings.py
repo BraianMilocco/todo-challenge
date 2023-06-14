7513a47a1a139bdec9b9a855b2e418cca9a0d979
Braian <braian.cnlp@gmail.com>
2023-06-14 09:29:16 -0300

    add some os.env variables

--- app/settings.py
+++ app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ax+3^g*0e!x3yq*)8mo%9dy7%__(^9(jie#4te3s%0%woa^pa#'
+SECRET_KEY = os.environ.get('secret_key', 'randomString1234')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.environ.get('debug', 'True') == 'True' else False
 
 ALLOWED_HOSTS = []
 
